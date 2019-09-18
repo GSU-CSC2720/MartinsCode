6d97e3b5306a7eb26ae50fcf2d8354618858d6f9
Martin Nguyen <martiann713@gmail.com>
2019-09-18 13:03:59 -0400

    random message

--- assignment 1/count/count.java
+++ assignment 1/count/count.java
@@ -2,6 +2,9 @@ import java.io.*;
 import java.util.*;
 public class count
 {
+
+    /** this is basic knowledge*/
+
     public static void main(String [] args)
     {
         int n = getInt("Please enter an integer value greater than or equal to 0");
